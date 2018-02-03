f0383856cf40c5d366d6f7e82c939c69d5a20f94
Strangebrewer <BKAShambala@gmail.com>
2018-02-03 02:40:13 -0700

    finished game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,91 @@
 // Defining variables
-var words = ["soundgarden", "beastie boys", "alice in chains", "pink floyd", "monster magnet", "tool", "nirvana", "live", "morcheeba", "beck", "modest mouse"];
+var words = [
+  "alice in chains"
+  , "beastie boys"
+  , "beck"
+  , "dead can dance"
+  , "inxs"
+  , "led zeppelin"
+  , "live"
+  , "modest mouse"
+  , "monster magnet"
+  , "morcheeba"
+  , "pink floyd"
+  , "prince"
+  , "soundgarden"
+  , "temple of the dog"
+  , "tool"
+];
+var names = [
+  "Alice In Chains"
+  , "Beastie Boys"
+  , "Beck"
+  , "Dead Can Dance"
+  , "INXS"
+  , "Led Zeppelin"
+  , "Live"
+  , "Modest Mouse"
+  , "Monster Magnet"
+  , "Morcheeba"
+  , "Pink Floyd"
+  , "Prince"
+  , "Soundgarden"
+  , "Temple of the Dog"
+  , "TooL"
+];
+var audio = [
+  "alice-audio"
+  , "beastie-audio"
+  , "beck-audio"
+  , "dance-audio"
+  , "inxs-audio"
+  , "zeppelin-audio"
+  , "live-audio"
+  , "mouse-audio"
+  , "monster-audio"
+  , "morcheeba-audio"
+  , "pink-audio"
+  , "prince-audio"
+  , "soundgarden-audio"
+  , "temple-audio"
+  , "tool-audio"
+];
+var images = [
+  "alice-image"
+  , "beastie-image"
+  , "beck-image"
+  , "dance-image"
+  , "inxs-image"
+  , "zeppelin-image"
+  , "live-image"
+  , "mouse-image"
+  , "monster-image"
+  , "morcheeba-image"
+  , "pink-image"
+  , "prince-image"
+  , "soundgarden-image"
+  , "temple-image"
+  , "tool-image"
+];
+var currentImage =
+  "<h4>" +
+  names[currentWordIndex] +
+  "</h4>" +
+  "<img class='current-image' src='" +
+  images[currentWordIndex] +
+  " alt='hangman image'>"
+  ;
 var letterNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
 var currentWord;
 var currentWordIndex;
 var currentWordArray = [];
+var previousWordIndex;
 var displayLoss = [];
 var blanksArray = [];
 var alreadyGuessed = [];
 var wins = 0;
 var losses = 0;
-var numDashes = 0;
+var numSpaces = 0;
 var remainingCorrectGuesses;
 var remainingWrongGuesses = 7;
 
@@ -17,15 +93,15 @@ var remainingWrongGuesses = 7;
 function newGame() {
   getLetters();
   hangmanGame();
-  remainingCorrectGuesses = currentWordArray.length - numDashes;
+  remainingCorrectGuesses = currentWordArray.length - numSpaces;
   document.getElementById("game-outcome").innerHTML = "";
+  document.getElementById("game-begin").style.animation = "none";
   document.getElementById("game-begin").innerHTML = "Good Luck!";
   document.getElementById("guesses-remaining").innerHTML = remainingWrongGuesses;
   document.getElementById("wins").innerHTML = wins;
   document.getElementById("losses").innerHTML = losses;
   document.getElementById("word-blanks").innerHTML = blanksArray.join(' ')
   document.getElementById("already-guessed").innerHTML = "";
-  // console.log(remainingCorrectGuesses);
 }
 
 //This function resets the arrays after each game
@@ -35,9 +111,10 @@ function reset() {
   blanksArray.length = 0;
   alreadyGuessed.length = 0;
   remainingWrongGuesses = 7;
-  numDashes = 0;
+  numSpaces = 0;
+  document.getElementById("game-outcome").style.animation = "none";
+  document.getElementById("game-outcome").style.animationIterationCount = "0";
 }
-
 
 // This function sets the currentWord variable, pushes each letter into the currentWordArray, and puts a blank into blanksArray for each letter, joining them into one string for display in the html:
 function getLetters() {
@@ -53,17 +130,18 @@ function getLetters() {
       displayLoss.push(currentWord.charAt(i));
       blanksArray.push("_");
     } else if (currentWord.charAt(i) === ' ') {
-      currentWordArray.push("-");
-      displayLoss.push("-");
-      blanksArray.push("-");
+      currentWordArray.push("&nbsp;");
+      displayLoss.push("&nbsp;");
+      blanksArray.push("&nbsp;");
     }
 
     if (currentWord.charAt(i) === " ") {
-      numDashes++;
+      numSpaces++;
     }
 
   }
 }
+
 
 // This is the main game function
 function hangmanGame() {
@@ -94,32 +172,46 @@ function hangmanGame() {
     for (i = 0; i < currentWordArray.length; i++) {
       if (letterNumbers.includes(userGuess) && userGuess === currentWordArray[i]) {
         blanksArray.splice(i, 1, currentWordArray[i]);
-        // the next line is necessary to prevent a win by pressing the same correct guess over and over again.
+        // the next line is necessary to prevent a win by pressing the same correct guess over and over again - because after it's been guessed, it's replaced in the array with a "0", and from that point on it won't decrement remainingCorrectGuesses.
         currentWordArray.splice(i, 1, "0");
         document.getElementById("word-blanks").innerHTML = blanksArray.join(' ');
         remainingCorrectGuesses--;
       }
     }
 
+    if (remainingCorrectGuesses === 0 || remainingWrongGuesses === 0) {
+      if (audio[previousWordIndex]) {
+        document.getElementById(audio[previousWordIndex]).pause();
+      }
+      if (images[previousWordIndex]) {
+        document.getElementById(images[previousWordIndex]).style.display = "none";
+      }
+      document.getElementById("default-image").style.display = "none";
+      document.getElementById("game-begin").innerHTML = "Press Any Key to Play Again";
+      document.getElementById("game-begin").style.animation = "opac .8s";
+      document.getElementById("game-begin").style.animationIterationCount = "infinite";
+      document.getElementById(audio[currentWordIndex]).play();
+      document.getElementById(images[currentWordIndex]).style.display = "block";
+      document.onkeyup = function () {
+        previousWordIndex = currentWordIndex;
+        reset();
+        newGame();
+      }
+    }
+
     if (remainingCorrectGuesses === 0) {
       wins++;
       document.getElementById("wins").innerHTML = wins;
-      document.getElementById("game-outcome").innerHTML = "YOU WIN!";
-      document.getElementById("game-begin").innerHTML = "Press Any Key to Play Again";
-      document.onkeyup = function () {
-        reset();
-        newGame();
-      }
+      document.getElementById("game-outcome").innerHTML = "YOU WON!";
+      document.getElementById("game-outcome").style.animation = "opac2 1.5s";
+      document.getElementById("game-outcome").style.animationIterationCount = "1";
     } else if (remainingWrongGuesses === 0) {
       losses++;
       document.getElementById("losses").innerHTML = losses;
-      document.getElementById("game-outcome").innerHTML = "YOU LOSE!";
-      document.getElementById("game-begin").innerHTML = "Press Any Key to Play Again";
+      document.getElementById("game-outcome").innerHTML = "YOU LOST!";
       document.getElementById("word-blanks").innerHTML = displayLoss.join(' ');
-      document.onkeyup = function () {
-        reset();
-        newGame();
-      }
+      document.getElementById("game-outcome").style.animation = "opac2 1.5s";
+      document.getElementById("game-outcome").style.animationIterationCount = "1";
     }
 
     document.getElementById("guesses-remaining").innerHTML = remainingWrongGuesses;
@@ -132,7 +224,3 @@ function hangmanGame() {
 document.onkeyup = function () {
   newGame();
 }
-
-
-// NEXT:
-// See if embedding the videos in the HTML with display: none and then triggering display: block makes it easier to autoplay. You can test this quickly by triggering wins and losses to just one video.
